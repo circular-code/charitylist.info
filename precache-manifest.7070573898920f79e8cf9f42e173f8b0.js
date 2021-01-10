0453defb2bd64abe4c83cc6a6f158bac731de2f0
Stephanos <stephanos@malikotsinas.de>
2021-01-10 17:24:24 +0100

    Updates

---
+++ precache-manifest.7070573898920f79e8cf9f42e173f8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad1632828e25e2a36eba5606953b7c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9cf31bc4be9992b4bcd",
+    "url": "/static/css/main.dc6e38ad.chunk.css"
+  },
+  {
+    "revision": "de9f3b6d0c3f66bacac9",
+    "url": "/static/js/2.ee59b570.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.ee59b570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cf31bc4be9992b4bcd",
+    "url": "/static/js/main.4381e164.chunk.js"
+  },
+  {
+    "revision": "d0af3f026a1402cf0b0d",
+    "url": "/static/js/runtime-main.ad42e892.js"
+  }
+]);
