213779f408bcfd6ad1a1804979e6c136f480dd5e
Stephanos <stephanos@malikotsinas.de>
2021-01-10 12:52:11 +0100

    tried another way of publishing on gh pages

---
+++ build/precache-manifest.76bf637c502d2bf69c69722633edcf5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6da6efe9f98a25bd8d36057c9501baf",
+    "url": "/charitylist.info/index.html"
+  },
+  {
+    "revision": "57c34145c8f8d2a102f5",
+    "url": "/charitylist.info/static/css/main.1e8718cc.chunk.css"
+  },
+  {
+    "revision": "9701969e08d3b9186ee6",
+    "url": "/charitylist.info/static/js/2.96bbaef6.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/charitylist.info/static/js/2.96bbaef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c34145c8f8d2a102f5",
+    "url": "/charitylist.info/static/js/main.f808b357.chunk.js"
+  },
+  {
+    "revision": "e0f1a0d7c74b0e2d4b0c",
+    "url": "/charitylist.info/static/js/runtime-main.5791ae27.js"
+  },
+  {
+    "revision": "fe4917e41c99c217964313f843c07bad",
+    "url": "/charitylist.info/static/media/your.fe4917e4.svg"
+  }
+]);
