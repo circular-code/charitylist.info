8552ed89fdeb96604cb716cfd018c0271666d348
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2021-01-15 14:35:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 //TODO: "DONE" Button after category selection
 //TODO: selected:true für qs übergebene regions
 //TODO: Daten direkt nach Value Änderung reloaden wenn Suchergebnisse offen?
+// $.post('/plugin/SynergySearch/Search/search', {offset:0, limit: 100, columns:["Name","Internet", "Kategorie"]})
 
 ReactDOM.render(
      <App/>,
